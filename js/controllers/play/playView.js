5378c25d387a27970bc701ea8de43caa8542142c
vbarrantesh <vbarrantesh@ucenfotec.ac.cr>
2024-04-07 02:05:18 -0600

    Css card style

--- js/controllers/play/playView.js
+++ js/controllers/play/playView.js
@@ -4,15 +4,36 @@ import { CardView } from "./cardView.js";
 
 export class PlayView extends BaseView {
     constructor(parent, controller) {
-        super(parent, controller);
+            super(parent, controller);
 
-        this.className = "playView";
-        this.cardsContainer = div({ className: 'playView-cardsContainer' }, this);
-        this.cardViews = [];
-    }
+            this.className = "playView";
+            this.cardsContainer = div({ className: 'playView-cardsContainer' }, this);
+            this.cardViews = [];
+        }
+        /* showCards(cards) {
+             cards.forEach(card => {
+                 this.cardViews.push(new CardView(this.cardsContainer, card, this.onCardSelected.bind(this)));
+             });
+         }*/
+
     showCards(cards) {
-        cards.forEach(card => {
-            this.cardViews.push(new CardView(this.cardsContainer, card, this.onCardSelected.bind(this)));
+        const cardsPerRow = 4;
+        let currentRow;
+
+        // Eliminar cualquier contenido previo en el contenedor
+        this.cardsContainer.innerHTML = '';
+
+        // Iterar sobre todas las tarjetas y agruparlas en filas
+        cards.forEach((card, index) => {
+            if (index % cardsPerRow === 0) {
+                // Comenzar una nueva fila
+                currentRow = div({ className: 'playView-cardRow' });
+                this.cardsContainer.appendChild(currentRow);
+            }
+            // Crear una instancia de CardView para representar la tarjeta
+            const cardView = new CardView(currentRow, card, this.onCardSelected.bind(this));
+            // Agregar la vista de la tarjeta al arreglo cardViews
+            this.cardViews.push(cardView);
         });
     }
 
